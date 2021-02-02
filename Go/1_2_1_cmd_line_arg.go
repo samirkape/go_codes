d36192106990cb16ce8c871503ac305155c510c3
samirkape <samirkape31@gmail.com>
2021-02-02 09:39:25 +0530

    CLA with range for loop

---
+++ Go/1_2_1_cmd_line_arg.go
@@ -0,0 +1,16 @@
+package main
+//this code prints the command line arguments passed using range for loop 
+
+import(
+	"os"
+	"fmt"
+)
+
+func main(){
+	var s,sep = "","" 
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
