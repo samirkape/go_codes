790f3aa45b21cd845e75d1857fe3d8511147a30f
samirkape <samirkape31@gmail.com>
2021-02-12 11:03:54 +0530

    Feb 12 2021 1_5_fetch_url_http_get -- fetch

---
+++ Go/1_5_fetch_url_http_get.go
@@ -0,0 +1,32 @@
+// this utility will fetch the URL specified on CL
+// refer >> man curl for more details
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	args := os.Args[1:]
+	for _, url := range args {
+		response, err := http.Get(url)
+		check_err(err, "http GET error")
+		response_body, err := ioutil.ReadAll(response.Body)
+		response.Body.Close()
+		check_err(err, "reading error")
+		fmt.Printf("%s ", response_body)
+	}
+
+}
+
+func check_err(err error, estr string) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fetch: %s %s ", estr, err)
+		os.Exit(-2)
+	}
+}
