88006220d0f6301492ee831b84b40299a0f6eb82
samirkape <samirkape31@gmail.com>
2021-02-12 11:53:12 +0530

    Feb 12 2021 -- Fetch the URL specified on CL with response.Status

---
+++ Go/1_5_ex_3_fetch_url_http_get.go
@@ -0,0 +1,43 @@
+// this utility will Fetch the URL specified on CL with response.Status
+// refer >> man curl for more details
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	args := os.Args[1:]
+	for _, _url := range args {
+		url := add_prefix(_url)
+		response, err := http.Get(url)
+		check_err(err, "http GET error")
+		io.Copy(os.Stdout, response.Body)
+		stat := "http response code: " + response.Status
+		fmt.Printf("%s\n", stat)
+		response.Body.Close()
+	}
+
+}
+
+func add_prefix(url string) string {
+	if strings.HasPrefix(url, "http://") {
+		return url
+	} else {
+		fmt.Println("http:// not found. adding...")
+		return "http://" + url
+	}
+}
+
+func check_err(err error, estr string) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fetch: %s %s ", estr, err)
+		os.Exit(-2)
+	}
+}
