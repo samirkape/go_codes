ece62d52e7c5c1b33359a594fea6405eba75c1dd
samirkape <samirkape31@gmail.com>
2021-03-30 10:08:29 +0530

    Mar 30 2021 -- Employee DS

---
+++ Go/ch4/struct/employee/s_employee.go
@@ -0,0 +1,17 @@
+// Fields related to employee
+
+package employee
+
+type Personal struct {
+	Name    string
+	Father  string
+	Sirname string
+	Address string
+	Age     int
+}
+
+type Official struct {
+	Id      string
+	Address string
+	Company string
+}
