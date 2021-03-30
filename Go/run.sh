f7885af966c05b99aa52314a7071307e08b97832
samirkape <samirkape31@gmail.com>
2021-03-30 11:17:13 +0530

    Mar 30 2021 -- Updated

--- Go/run.sh
+++ Go/run.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-unset name
-
-while getopts "c:b:r:d:" opt; do
-
-case $opt in
-
-b)
-go build $OPTARG 
-mv ${OPTARG%.*} build/
-echo "Build Successful"
-;;
-
-r)
-go build $OPTARG 
+go build $1
 result=$?
 if [ $result -ne 0 ]
 then
@@ -21,23 +8,7 @@ then
     echo "error occured!"
     exit $result
 fi
-mv `basename ${OPTARG%.*}` build/
+mv `basename ${1%.*}` build/
 echo "Build Successful"
-./build/`basename ${OPTARG%.*}` $3
-;;
+./build/`basename ${1%.*}` $2
 
-d)
-if [ -f build/${OPTARG%.*} ]
-then
-    rm build/${OPTARG%.*}
-    echo "Removed!"
-fi
-;;
-
-*)
-#Printing error message
-echo "invalid option or argument $OPTARG"
-;;
-
-esac
-done
