9eeb0cde919ee642948d11a92cb768992b71297b
samirkape <samirkape31@gmail.com>
2021-03-31 09:51:53 +0530

    Mar 31 2021 -- Trying JSON Marshal/MarshalIndent

---
+++ Go/ch4/JSON/encode.go
@@ -0,0 +1,42 @@
+// Trying JSON encoding with Go
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Movie struct {
+	Title  string
+	Year   int  `json:"released"`
+	Color  bool `json: "color,omitempty"`
+	Actors []string
+}
+
+var movies = []Movie{
+	{Title: "Casanova", Year: 2005, Color: true, Actors: []string{"Heath Ledger"}},
+	{Title: "Casablanca", Year: 1942, Color: false, Actors: []string{"Humphrey Bogart", "Ingrid Bergman"}},
+}
+
+func main() {
+	Marshal()
+	Print()
+}
+
+func Marshal() {
+	data, err := json.Marshal(movies)
+	if err != nil {
+		log.Fatalf("JSON marshaling failed: %s", err)
+	}
+	fmt.Printf("%s\n", data)
+}
+
+func Print() {
+	data, err := json.MarshalIndent(movies, "", " ")
+	if err != nil {
+		log.Fatalf("JSON marshaling failed: %s", err)
+	}
+	fmt.Printf("%s\n", data)
+}
