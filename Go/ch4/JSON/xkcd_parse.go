169a382a64f88ea29bda874136742b13a80e9390
samirkape <samirkape31@gmail.com>
2021-04-04 17:41:57 +0530

    Apr 04 2021 -- Phase 1: Successfully written xkcd transcript into local mongoDB

--- Go/ch4/JSON/xkcd_parse.go
+++ Go/ch4/JSON/xkcd_parse.go
@@ -1,4 +1,10 @@
-// Trying JSON encoding with Go
+/* Exercise 4.12: The popular web comic xkcd has a JSON interface.
+For example, a request to https://xkcd.com/571/info.0.json produces a detailed description of comic 571, one of many favorites.
+Download each URL (once!) and build an offline index.
+Write a tool xkcd that, using this index,
+    1. prints the URL
+    2. transcript of each comic that matches a search term provided on the command line.
+*/
 
 package main
 
