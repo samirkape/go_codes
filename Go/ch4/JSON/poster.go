f67e90fd7254f033a47d757b5bab12cd4711b744
samirkape <samirkape31@gmail.com>
2021-04-05 11:07:49 +0530

    Apr 05 2021 -- The JSON-based web service of the Open Movie Database lets you search https://omdbapi.com/ for a movie by name and download its poster image. Write a tool poster that downloads the poster image for the movie named on the command line.

--- Go/ch4/JSON/poster.go
+++ Go/ch4/JSON/poster.go
@@ -5,9 +5,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
+	"strings"
 )
 
 type Movie struct {
@@ -18,14 +21,11 @@ var URL = "https://omdbapi.com/"
 var APIKey = "3e8be282"
 
 func main() {
-	// MovieName := os.Args[1]
-	MovieName := "casanova"
-	// col := InitDB()
-	//DeleteCollection(col)
+	MovieName := os.Args[1]
+	//MovieName := "casanova"
 	url := BuildQuery(MovieName)
 	PosterURL := FetchPosterURL(url)
-	fmt.Printf("\tURL\t\t\ttranscript\n")
-	fmt.Println(PosterURL)
+	WriteFile(MovieName, PosterURL)
 }
 
 func FetchPosterURL(rawurl string) string {
@@ -39,6 +39,7 @@ func FetchPosterURL(rawurl string) string {
 		response.Body.Close()
 		log.Fatal(err)
 	}
+	response.Body.Close()
 	return PosterURL.Poster
 }
 
@@ -48,12 +49,39 @@ func RaiseFatal(err error) {
 	}
 }
 
+func WriteFile(MovieName, PosterURL string) {
+
+	EmptyFile, FileName := CreateFile(MovieName)
+	FetchImg(EmptyFile, PosterURL)
+	defer EmptyFile.Close()
+	fmt.Printf("\tDownload Successful: %s\n", FileName)
+}
+
+func CreateFile(MovieName string) (*os.File, string) {
+	filename := strings.ReplaceAll(MovieName, " ", "+")
+	filename += ".jpg"
+	file, err := os.Create(filename)
+	RaiseFatal(err)
+	return file, filename
+}
+
+func FetchImg(File *os.File, PosterURL string) {
+	response, err := http.Get(PosterURL)
+	if err != nil {
+		response.Body.Close()
+		log.Fatal(err)
+	}
+	_, err = io.Copy(File, response.Body)
+	if err != nil {
+		response.Body.Close()
+		log.Fatal(err)
+	}
+	response.Body.Close()
+}
+
 func BuildQuery(MovieName string) string {
 	base, err := url.Parse(URL)
 	RaiseFatal(err)
-	// Path params
-	//base.Path += "this will get automatically encoded"
-	// Query params
 	params := url.Values{}
 	params.Add("t", MovieName)
 	params.Add("apikey", APIKey)
