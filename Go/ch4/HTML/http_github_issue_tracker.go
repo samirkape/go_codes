853404f7db738917458149d17c9b9e5ee56c1853
samirkape <samirkape31@gmail.com>
2021-04-07 09:05:03 +0530

    Apr 07 2021 -- Added Hyperlinks: Moved author field to different struct in HTML template

--- Go/ch4/HTML/http_github_issue_tracker.go
+++ Go/ch4/HTML/http_github_issue_tracker.go
@@ -123,23 +123,3 @@ func SearchIssues(terms []string) (*IssuesSearchResult, error) {
 	resp.Body.Close()
 	return &result, nil
 }
-
-func GetStarred() (*IssuesSearchResult, error) {
-	resp, err := http.Get(Starred)
-	if err != nil {
-		return nil, err
-	}
-	// We must close resp.Body on all execution paths.
-	// (Chapter 5 presents 'defer', which makes this simpler.)
-	if resp.StatusCode != http.StatusOK {
-		resp.Body.Close()
-		return nil, fmt.Errorf("search query failed: %s", resp.Status)
-	}
-	var result IssuesSearchResult
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		resp.Body.Close()
-		return nil, err
-	}
-	resp.Body.Close()
-	return &result, nil
-}
