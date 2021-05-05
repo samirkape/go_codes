acaaf7ac3d82596f79651fd2e35f898b818318a9
Samir Kape <samirkape31@gmail.com>
2021-05-05 09:23:47 +0530

    May 05 2021 -- Creating a pointer receiver for converting termperature from Fahrenheit to Celsius

--- Go/ch6/method_receiver_ptr.go
+++ Go/ch6/method_receiver_ptr.go
@@ -9,12 +9,15 @@ func main() {
 }
 
 type Fahr []float64
+type Cels []float64
 
 func tempConv() {
 	F := Fahr{103, 102, 100}
 	fmt.Printf("%p\n", &F)
-	F.Celsius()
+	C := F.CelsiusR() // Getting C in the return, preserving F
+	F.Celsius()       // Modifying F
 	fmt.Println(F)
+	fmt.Println(C)
 	fmt.Printf("%T\n", F)
 }
 
@@ -25,4 +28,13 @@ func (F *Fahr) Celsius() {
 		F1[i] = (F1[i] - 32) * 9 / 5
 	}
 	F = &F1
+}
+
+func (F Fahr) CelsiusR() (C Cels) { // Named return too need allocation before using
+	C = make(Cels, len(F))
+	fmt.Printf("%p\n", F)
+	for i, f := range F {
+		C[i] = (f - 32) * 9 / 5
+	}
+	return
 }
