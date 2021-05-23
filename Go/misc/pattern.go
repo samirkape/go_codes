116063c63412ab27388a260ce4b2f7d736911d76
Samir Kape <samirkape31@gmail.com>
2021-05-23 12:01:33 +0530

    May 23 2021 -- Leuwint pattern

---
+++ Go/misc/pattern.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func flip( toggle *int ) {
+	if *toggle == 0 {
+		*toggle = 1
+	} else {
+		*toggle = 0
+	}
+}
+
+const INNER = 21
+const OUTER = 11
+
+func main() {
+	pattern := ""
+	toggle := 0
+	inner := INNER
+	outer := OUTER
+	spaces := ""
+
+	for i := 0; i < outer; i++ {
+		for j := 0; j < inner; j++ {
+			if inner != INNER {
+				spaces = strings.Repeat(" ", (INNER - inner) / 2.0)
+			}
+			flip(&toggle)
+			pattern = pattern + fmt.Sprintf("%d", toggle)
+		}
+		inner -= 2
+		fmt.Println(spaces + pattern)
+		pattern = ""
+		flip(&toggle)
+	}
+}
---
+++ Go/misc/r_bot/go.mod
@@ -0,0 +1,3 @@
+module r_bot
+
+go 1.16
