ad73afdc4ae4975b248e71a5c07ee7beab74f790
samirkape <samirkape31@gmail.com>
2021-06-02 17:42:52 +0530

    Jun 02 2021 -- copy of io.Writer

---
+++ Go/ch7/generic_print.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+)
+
+type Storage struct {
+	buf []byte
+}
+
+func (s *Storage) Write(p []byte) (int, error) {
+	s.buf = p
+	return len(s.buf), nil
+}
+
+func Printf(format string, args ...interface{}) (int, error) {
+	return fmt.Fprintf(os.Stdout, format, args...) // Writing to stdout
+}
+
+func Sprintf(format string, args ...interface{}) string {
+	var buf bytes.Buffer
+	fmt.Fprintf(&buf, format, args...) //
+	return buf.String()
+}
+
+func eSprintf(format string, args ...interface{}) string {
+	var buffer Storage
+	fmt.Fprintf(&buffer, format, args...) //
+	return string(buffer.buf)
+}
+
+func main() {
+	fmt.Println(eSprintf("Samir %s", "Kape"))
+}
