d8b886073c4793f2e88b9f40a9b341e5cd8fc112
samirkape <samirkape31@gmail.com>
2021-08-07 16:47:17 +0530

    Aug 07 2021

---
+++ Go/scratchpad/gin.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/person/:id", catelog)
+	r.Run()
+}
+
+func catelog(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		c.: "User found",
+	})
+}
