9c3b34d50bd9659b1ff2a528c2c329c8e822f236
samirkape <samir.kape@gslab.com>
2021-08-07 16:24:05 +0530

    Aug 07 2021

--- Go/scratchpad/service.go
+++ Go/scratchpad/service.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 )
 
 func Service() bool {
@@ -10,23 +11,44 @@ func Service() bool {
 	return r[0] > 2
 }
 
-func GetService() {
-	MAX_QUEUE := 10
-	status := make(chan bool, MAX_QUEUE)
+func GetService(pool *sync.Pool, MAX_QUEUE int) {
 
-	for i := 0; i < MAX_QUEUE; i++ {
-		status <- Service()
-	}
+	var status chan bool
 
 	go func() {
 		for i := 0; i < MAX_QUEUE; i++ {
 			fmt.Println(<-status)
 		}
+		defer pool.Put(status)
+
+	}()
+
+	func() {
+		status = pool.Get().(chan bool)
+		for i := 0; i < MAX_QUEUE; i++ {
+			status <- Service()
+		}
 	}()
 }
 
 func main() {
+	var numPoolCreated int = 1
+	MAX_QUEUE := 1000
+
+	pool := &sync.Pool{
+		New: func() interface{} {
+			status := make(chan bool, numPoolCreated*MAX_QUEUE)
+			numPoolCreated++
+			return status
+		},
+	}
+
+	pool.Put(pool.New())
+	pool.Put(pool.New())
+	pool.Put(pool.New())
+	pool.Put(pool.New())
+
 	for {
-		GetService()
+		GetService(pool, numPoolCreated*MAX_QUEUE)
 	}
 }
