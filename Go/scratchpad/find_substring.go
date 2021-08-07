e8870b20905a08cd11f3bc34740a29fc3e763840
samirkape <samir.kape@gslab.com>
2021-08-07 16:19:54 +0530

    Aug 07 2021

---
+++ Go/scratchpad/find_substring.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func FindIndex(sen1, sen2 string) int {
+	var record []int
+	var foundFlag bool = true
+	var tmpIndex int
+
+	s1 := strings.Fields(sen1)
+	s2 := strings.Fields(sen2)
+
+	for index := 0; index < len(s1); index++ {
+		s1token := s1[index]
+		if s1token == s2[0] {
+			tmpIndex = index
+			j := index + 1
+			for i := 1; i < len(s2); i++ {
+				if s1[j] != s2[i] {
+					foundFlag = false
+					index = j
+					break
+				}
+				j++
+			}
+			if foundFlag {
+				record = append(record, tmpIndex)
+				index = j - 1
+			}
+			foundFlag = true
+		}
+	}
+
+	if len(record) == 0 {
+		return -1
+	}
+
+	sort.Ints(record)
+
+	return record[len(record)-1]
+}
+
+func main() {
+	sen1 := `some people over age 60 have few indicate hypothyroidism in an if any, symptoms of an some people underactive thyroid gland (hypothyroidism), while others experience the same symptoms some people younger people do. Still others have hypothyroidism symptoms that are not typical at all, making the diagnosis even more difficult. Any of the following signs and symptoms can indicate hypothyroidism in an older person. `
+	sen2 := "some people"
+	f := strings.Fields(sen1)
+	_ = f
+	index := FindIndex(sen1, sen2)
+	fmt.Println(index)
+}
